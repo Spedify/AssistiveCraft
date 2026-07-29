1f12ce781c0c56583aa644113d6777ef30ae0556
Spedify <148723891+Spedify@users.noreply.github.com>
2026-07-29 12:58:18 -0700

    Update KeyBindings.java

--- src/main/java/com/assistivecraft/KeyBindings.java
+++ src/main/java/com/assistivecraft/KeyBindings.java
@@ -1,27 +1,43 @@
 package com.assistivecraft;
 
 import com.assistivecraft.gui.AssistiveGuiScreen;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
-    public static KeyBinding openGuiKey;
+    public static KeyBinding guiKey;
+    public static KeyBinding eatKey;
+    public static KeyBinding totemKey;
 
     public static void register() {
-        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.assistivecraft.opengui",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_GRAVE_ACCENT, // Grave Accent (`) key
-            "category.assistivecraft.general"
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.assistivecraft.gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_GRAVE_ACCENT,
+                "category.assistivecraft.main"
         ));
-    }
 
-    public static void checkKeys(MinecraftClient client) {
-        while (openGuiKey.wasPressed()) {
-            client.setScreen(new AssistiveGuiScreen());
-        }
+        eatKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.assistivecraft.eat",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_V,
+                "category.assistivecraft.main"
+        ));
+
+        totemKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.assistivecraft.totem",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                "category.assistivecraft.main"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (guiKey.wasPressed()) {
+                client.setScreen(new AssistiveGuiScreen());
+            }
+        });
     }
 }
