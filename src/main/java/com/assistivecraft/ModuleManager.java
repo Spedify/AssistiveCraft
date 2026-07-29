8db1ef5e33eb424a0cc27cef9fdf2e338a715e33
Spedify <148723891+Spedify@users.noreply.github.com>
2026-07-29 11:27:49 -0700

    Update ModuleManager.java

--- src/main/java/com/assistivecraft/ModuleManager.java
+++ src/main/java/com/assistivecraft/ModuleManager.java
@@ -1,12 +1,34 @@
 package com.assistivecraft;
 
 public class ModuleManager {
-    public static boolean entityOutlineEnabled = true;
-    public static boolean oreOutlineEnabled = true;
-    public static boolean projectilePathEnabled = true;
+    public static final ModuleManager INSTANCE = new ModuleManager();
 
-    public static boolean autoEatEnabled = true;
-    public static boolean combatAssistEnabled = true;
-    public static boolean fallMitigationEnabled = true;
-    public static boolean totemSwapEnabled = true;
+    // --- CATEGORY DROPDOWN EXPANSION STATES ---
+    public boolean combatCategoryExpanded = true;
+    public boolean outlinesCategoryExpanded = true;
+    public boolean utilityCategoryExpanded = true;
+
+    // --- COMBAT INDIVIDUAL TOGGLES & SETTINGS ---
+    public boolean crosshairAssistEnabled = false;
+    public float crosshairAssistSpeed = 0.15f;
+    public float crosshairAssistFovDegrees = 60.0f;
+    
+    public boolean attackSyncEnabled = false;
+    public double interactionDistance = 4.5;
+    
+    public boolean totemSwapEnabled = false;
+
+    // --- OUTLINES INDIVIDUAL TOGGLES & SETTINGS ---
+    public boolean entityOutlinesEnabled = false;
+    
+    public boolean oreOutlinesEnabled = false;
+    public int oreOutlineRadius = 16;
+    
+    public boolean projectilePathEnabled = false;
+
+    // --- UTILITY INDIVIDUAL TOGGLES & SETTINGS ---
+    public boolean autoEatEnabled = false;
+    public int autoEatHungerThreshold = 14;
+    
+    public boolean fallMitigationEnabled = false;
 }
